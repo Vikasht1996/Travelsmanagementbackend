5974c1b6bac27df49bf7de487e40bd6cba1e5192
Vikasvics <vikasvics19@gmail.com>
2019-08-14 14:49:19 +0530

    Commit14

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -61,18 +61,20 @@ exports.signup = function(req, res){
         User.findOne({Email:Email})
         .then(user =>{
         if(!user){
-        const error = new Error('A user with this email could not be found.');
-        error.statusCode = 401;
-        throw error;
+          res.send("User does not exist")
+        // const error = new Error('A user with this email could not be found.');
+        // error.statusCode = 401;
+        // throw error;
         }
         loadedUser = user;
         return bcrypt.compare(Password,user.Password);
         })
         .then(isEqual =>{
         if(!isEqual){
-        const error = new Error('wrong password.');
-        error.statusCode = 401;
-        throw error;
+          res.send("wrong password")
+        // const error = new Error('wrong password.');
+        // error.statusCode = 401;
+        // throw error;
         }
         const token = jwt.sign(
         {
--- server.js
+++ server.js
@@ -3,12 +3,26 @@ import routes from './api/routes/userRouter'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import cors from 'cors';
-
-
 const app = express()
 const PORT = 8000
+// const app = require("express")();
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+app.use(require("body-parser").text());
+app.post("/charge", async (req, res) => {
+    try {
+      let {status} = await stripe.charges.create({
+        amount: 2000,
+        currency: "usd",
+        description: "An example charge",
+        source: req.body
+      });
+  
+      res.json({status});
+    } catch (err) {
+      res.status(500).end();
+    }
+  });
 
- 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/travelmanagement')
  
